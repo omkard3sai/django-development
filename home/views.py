f3657b19c4cf72fbaf568e54a6c8bf5c830135a6
omkard3sai <omkar.desai@utdallas.edu>
2016-02-29 14:57:30 -0600

    Commit 1.1

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def index(request):
     html = "<h1>HELLO WORLD</h1><br>"
-    html += "<h4>Project View</h4>"
+    html += "<h4>Home View</h4>"
     return HttpResponse(html)
