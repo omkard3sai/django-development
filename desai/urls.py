54ca4a0ce42536182b8ee063663cb24e83cc007d
omkard3sai <omkar.desai@utdallas.edu>
2016-02-29 15:00:44 -0600

    Commit 1.2

--- desai/urls.py
+++ desai/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^home/', include('home.urls')),
     url(r'^projects/', include('projects.urls')),
     url(r'^admin/', admin.site.urls),
 ]
